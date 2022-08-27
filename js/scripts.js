dcc23eb942f1973041173b7cdaa205bf4b5a2209
Bai Jaitrong <bjaitrong@comcast.net>
2022-08-27 17:55:57 -0600

    Add a submit event Handler and function to read input values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 //User Interface Logic
 
-function handleRadio(event){
-  form.addEventListener("submit",function(even) {
-    const 
-  }
+function handleRadio(event) {
+  event.preventDefault();
+  const companySize = document.querySelector("input[name='companySize']:checked").value;
+  const color = document.querySelector("input[name='color']:checked").value;
+  const superHero = document.querySelector("input[name='superHero':checked").value;
+  const data = document.querySelector("input[name='data':checked").value;
+  const starWarsFan = document.querySelector("input[name='starWarsFan':checked").vlaue; 
 }
 
 window.addEventListener("load", function() {
-  let form = document.querySelector("form");
+  this.document.getElementById("radio-form").addEventListener("submit", handleRadio);
   let resetBtn = document.querySelector("button#reset");
   let codeLanguage = document.querySelector("div#codeLanguage")
-
-
 });
