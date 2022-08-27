d1ee6d4825ec3d340ed714aea342d7516add5686
Bai Jaitrong <bjaitrong@comcast.net>
2022-08-27 17:05:21 -0600

    Add columns to questions.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//User Interface Logic
+
+function handleRadio(event){
+  form.addEventListener("submit",function(even) {
+    const 
+  }
+}
+
+window.addEventListener("load", function() {
+  let form = document.querySelector("form");
+  let resetBtn = document.querySelector("button#reset");
+  let codeLanguage = document.querySelector("div#codeLanguage")
+
+
+});
