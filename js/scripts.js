95e4bc5254ac244ea3fc47f3cb2c96502328a163
Bai Jaitrong <bjaitrong@comcast.net>
2022-08-27 19:12:23 -0600

    WIP: Add Branches in js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,35 @@
 //User Interface Logic
 
-function handleRadio(event) {
-  event.preventDefault();
+function getAndEvaluateInputs {
   const companySize = document.querySelector("input[name='companySize']:checked").value;
   const color = document.querySelector("input[name='color']:checked").value;
-  const superHero = document.querySelector("input[name='superHero':checked").value;
-  const data = document.querySelector("input[name='data':checked").value;
-  const starWarsFan = document.querySelector("input[name='starWarsFan':checked").vlaue; 
+  const superHero = document.querySelector("input[name='superHero']:checked").value;
+  const data = document.querySelector("input[name='data']:checked").value;
+  const starWarsFan = document.querySelector("input[name='starWarsFan']:checked").vlaue; 
+
+  if(companySize === "largeCorporations") {
+
+  } else if (color === "red") {
+
+  } else if (superHero === "flash") {
+
+  } else if (data === "yes") {
+
+  } else if (starWarsFan == "yes") {
+
+  } else {
+
+  }
+}
+
+
+function handleRadio(event) {
+  event.preventDefault();
+  getAndEvaluateInputs();
 }
 
 window.addEventListener("load", function() {
-  this.document.getElementById("radio-form").addEventListener("submit", handleRadio);
+  document.getElementById("radio-form").addEventListener("submit", handleRadio);
   let resetBtn = document.querySelector("button#reset");
   let codeLanguage = document.querySelector("div#codeLanguage")
 });
